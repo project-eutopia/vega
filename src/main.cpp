fda4710b89b5233a5bd659a9eeef13ae595c1658
Christopher Locke <project.eutopia@gmail.com>
2018-07-23 13:00:52 +0900

    Dictionary has default filename
    Also update to use unique_ptr internally

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include "vega/controller.h"
 
 int main(int argc, char* argv[]) {
-  vega::dictionary::Dictionary::set_dictionary("/usr/local/share/vega/dictionary.txt");
   vega::Controller controller(argc, argv);
   controller.run();
 }
